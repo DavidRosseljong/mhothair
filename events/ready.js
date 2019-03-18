d9e05adc5805a169446610ba8045fdf561972a9c
David Rosseljong <david@david-rosseljong.com>
2019-03-18 09:58:15 +0100

    Setting activity

--- events/ready.js
+++ events/ready.js
@@ -2,4 +2,5 @@ module.exports = (client) => {
   console.log('Login initiated. Please wait...');
   console.log('Connection successful.');
   console.log(`${client.user.username} is online.`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers.`);
 };
