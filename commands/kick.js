97258fc08a33ca4ba14002a9be4bd08dbd3f0396
David Rosseljong <david@david-rosseljong.com>
2019-03-18 09:46:20 +0100

    Added admin role from my server

--- commands/kick.js
+++ commands/kick.js
@@ -1,25 +1,26 @@
 module.exports.run = async (client, msg, args) => {
 
-    if( !msg.member.roles.some(r => ["Administrator", "Admin", "Moderator", "Mod"].includes(r.name)) )
-      return msg.reply("Sorry, you don't have permissions to use this!");
-    
-    let member = msg.mentions.members.first() || msg.guild.members.get(args[0]);
+  // 👑 Admin role for my server.
+  if( !msg.member.roles.some(r => ["Administrator", "Admin", "Moderator", "Mod", "👑 Admin"].includes(r.name)) )
+    return msg.reply("Sorry, you don't have permissions to use this!");
+  
+  let member = msg.mentions.members.first() || msg.guild.members.get(args[0]);
 
-    if( !member )
-      return msg.reply("Please mention a valid member of this server.");
+  if( !member )
+    return msg.reply("Please mention a valid member of this server.");
 
-    if( !member.kickable ) 
-      return msg.reply("I can not kick this user! Do they have a higher role? Do I have kick permissions?");
-    
-    let reason = args.slice(1).join(' ');
+  if( !member.kickable ) 
+    return msg.reply("I can not kick this user! Do they have a higher role? Do I have kick permissions?");
+  
+  let reason = args.slice(1).join(' ');
 
-    if( !reason ) reason = "No reason provided.";
-    
+  if( !reason ) reason = "No reason provided.";
+  
 
-    await member.kick(reason)
-      .catch(error => msg.reply(`Sorry ${msg.author} I couldn't kick because of : ${error}.`));
+  await member.kick(reason)
+    .catch(error => msg.reply(`Sorry ${msg.author} I couldn't kick because of : ${error}.`));
 
-    msg.reply(`${member.user.tag} has been kicked by ${msg.author.tag} because: ${reason}.`);
+  msg.reply(`${member.user.tag} has been kicked by ${msg.author.tag} because: ${reason}.`);
 
-    msg.delete(15000);
+  msg.delete(15000);
 }
