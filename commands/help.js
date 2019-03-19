dca1bdf9bf7e5666375a5043ac1d72443bacc1f5
David <david@david-rosseljong.com>
2019-03-19 18:36:51 +0100

    Testing awaitMessages fn

--- commands/help.js
+++ commands/help.js
@@ -1,20 +1,17 @@
 module.exports.run = async (client, msg, args) => {
 
+  //Message to send
+  const embed = new Discord.RichEmbed()
+    .settitle('Social Media')
+    .addBlankField()
+    .addField('Facebook', 'www.facebook.com/ValixxOnline')
+    .addField('Twitter', 'www.twitter.com/ValixxOnline')
+    .setFooter(timestamp);
+
   await msg.delete().catch(O_o => {});
 
-  msg.reply('In what matter do you need assistance?');
-
-    const answer_social = msg.startsWith('social');
-
-    await answer_social().catch((err) => { console.log(err)})
-
-      const embed = new Discord.RichEmbed()
-        .settitle('Social Media')
-        .addBlankField()
-        .addField('Facebook', 'www.facebook.com/ValixxOnline')
-        .addField('Twitter', 'www.twitter.com/ValixxOnline')
-        .setFooter(timestamp);
-        
-      msg.reply({ embed });
+  await msg.channel.awaitMessages(message => {
+    console.log(msg.content);
+  });
 
 };
