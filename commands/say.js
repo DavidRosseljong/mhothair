07121661ccf10a6a13b3b810dcfb7f713af844e6
David <david@david-rosseljong.com>
2019-03-19 17:43:20 +0100

    Added comments for easy reading

--- commands/say.js
+++ commands/say.js
@@ -1,9 +1,12 @@
 module.exports.run = async (client, msg, args) => {
 
+  // Saving the message from user
   const newMessage = args.join(" ");
 
+  // Deleting the command
   msg.delete().catch(O_o=>{});
 
+  // Send the message to the channel as bot
   msg.channel.send(newMessage);
 
 };
