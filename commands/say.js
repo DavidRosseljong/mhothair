ddb70d84157298dc78f8f1b55fb051eb2f130574
David <david@david-rosseljong.com>
2019-03-19 14:39:23 +0100

    Hotfix #2 for say command

--- commands/say.js
+++ commands/say.js
@@ -1,9 +1,12 @@
 module.exports = (client, msg, args) => {
 
+  let messageArray = msg.content.split(/\s+/g);
+  let args = messageArray.slice(1);
+
   const message = args.join(" ");
 
   msg.delete().catch(O_o=>{});
 
   msg.channel.send(message);
 
-}
+};
