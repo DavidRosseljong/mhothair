877eef9f09ff992643bb15d23e71edde4674624c
David <david@david-rosseljong.com>
2019-03-19 16:54:01 +0100

    Fix for say command

--- commands/say.js
+++ commands/say.js
@@ -1,8 +1,5 @@
 module.exports = (client, msg, args) => {
 
-  let messageArray = msg.content.split(/\s+/g);
-  let args = messageArray.slice(1);
-
   const newMessage = args.join(" ");
 
   msg.delete().catch(O_o=>{});
