5fbb62d868494d0aef65976419cbd9981535d709
David <david@david-rosseljong.com>
2019-03-19 13:45:25 +0100

    added user size

--- events/ready.js
+++ events/ready.js
@@ -2,5 +2,5 @@ module.exports = (client) => {
   console.log('Login initiated. Please wait...');
   console.log('Connection successful.');
   console.log(`${client.user.username} is online.`);
-  client.user.setActivity(`Serving ${client.guilds.size} servers.`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers and ${client.users.size} users.`);
 };
