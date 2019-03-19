42039837b4eed83c1d6f1d562600506c07af17e8
David <david@david-rosseljong.com>
2019-03-19 20:45:59 +0100

    Output last user message, added more embeds

--- commands/help.js
+++ commands/help.js
@@ -2,18 +2,56 @@ module.exports.run = async (client, msg, args) => {
 
   const Discord = require('discord.js');
   
-  //Message to send
-  const embed = new Discord.RichEmbed()
+  //Message to send | Help Keywords
+  const helpKeywords = new Discord.RichEmbed()
+    .setTitle('Keywords')
+    .setDescription('Please use any of the keywords below.')
+    .addBlankField()
+    .addField('Social Media', 'social')
+    .addField('A List of all my mods', 'mods')
+    .setFooter(timestamp)
+
+  //Message to send | Social Media Links
+  const socialMedia = new Discord.RichEmbed()
     .setTitle('Social Media')
     .addBlankField()
     .addField('Facebook', 'www.facebook.com/ValixxOnline')
     .addField('Twitter', 'www.twitter.com/ValixxOnline')
+    .setFooter(timestamp)
+
+  //Message to send | Mod Links
+  const modLinks = new Discord.RichEmbed()
+    .setTitle('Mod Links')
+    .addBlankField()
+    .addField('RIFT')
+    .addField('MofixUI', 'www.facebook.com/ValixxOnline')
+    .addField('Notepad Reloaded', 'www.twitter.com/ValixxOnline')
+    .addBlankField()
+    .addField('LIF: Forest Village')
+    .addField('More Wood per Tree', 'www.valixx-online.de/more-wood-per-tree')
+    .setFooter(timestamp)
 
   await msg.delete().catch(O_o => {});
 
+  msg.reply(helpKeywords);
+
   await msg.channel.awaitMessages(message => {
-    console.log(message);
-    console.log(message.content);
+
+    switch(message.content) {
+
+      case 'social':
+        msg.reply(socialMedia)
+        break;
+
+      case 'mods':
+        msg.reply(modLinks);
+        break;
+
+      default:
+        msg.reply('You did not type correctly.');
+
+    };
+
   });
 
 };
