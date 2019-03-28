3d2253b9cc10b81247af05ecf221a2f31c130fb0
David <david@david-rosseljong.com>
2019-03-28 17:46:42 +0100

    Cleaned the code and switching to settings.js

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
 const { Client, Collection } = require('discord.js');
-const { settings } = require('./inc/settings');
-
-const config = require('./config.json');
 const client = new Client();
 
 const env = require('dotenv');
 env.config();
 
 
-client.config = config;
-client.settings = settings;
 client.commands = new Collection();
-
-
 client.commands.set('help', require('./commands/help.js'));
 client.commands.set('rules', require('./commands/rules.js'));
 client.commands.set('purge', require('./commands/purge.js'));
@@ -20,10 +13,8 @@ client.commands.set('kick', require('./commands/kick.js'));
 client.commands.set('say', require('./commands/say.js'));
 
 
-
 client.on('message', msg => require('./events/message.js')(client, msg));
 client.once('ready', () => require('./events/ready.js')(client));
 
 
-//client.login(config.key);
 client.login(process.env.BOT_TOKEN);
