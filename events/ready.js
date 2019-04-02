f99fa578cbe126c32a4ebcca4e3add0bcbd83aed
David <david@david-rosseljong.com>
2019-04-02 17:02:18 +0200

    Connect to database if ready

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,12 @@
 module.exports = (client) => {
+
+  const { connection, connect } = require('../inc/connection');
+
+  connect();
+
   console.log('Login initiated. Please wait...');
   console.log('Connection successful.');
   console.log(`${client.user.username} is online.`);
+
   client.user.setActivity(`Serving ${client.guilds.size} servers and ${client.users.size} users.`);
 };
