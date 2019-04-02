b03dd1cf94dd43d9eb8e8273d8242d3b2e498fd2
David <david@david-rosseljong.com>
2019-04-02 17:01:38 +0200

    XP command

--- commands/xp.js
+++ commands/xp.js
@@ -1,14 +1,7 @@
-module.exports.run = async (client, msg, args, con) => {
+module.exports.run = async (client, msg, args) => {
 
-  let target = msg.mentions.users.first() || msg.guild.members.get(args[1]) || msg.author;
+  const { xp } = require('../inc/connection');
 
-  connection.query(`SELECT * FROM xp WHERE id = '${target.id}'`, (err, rows) => {
-    if (err) throw err;
-
-    if (!rows[0]) return msg.channel.send('This User has not gathered any XP.');
-
-    let xp = rows[0].xp;
-    msg.channel.send(xp);
-  });
+  xp(msg, args);
 
 };
