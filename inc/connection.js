2c28358dbe41e2198eaea280e05e41469e2edc10
David <david@david-rosseljong.com>
2019-04-02 17:02:34 +0200

    mysql connection

--- inc/connection.js
+++ inc/connection.js
@@ -1,9 +1,11 @@
 const mysql = require('mysql');
 const { generate_xp } = require('./generate_xp');
 
+let connection = null;
 
-function connection() {
-  const connection = mysql.createConnection({
+
+function connect() {
+  connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
@@ -14,13 +16,16 @@ function connection() {
     if (err) throw err;
     console.log('Database connected.')
   });
+};
+
+function query(msg) {
 
   connection.query(`SELECT * FROM xp WHERE id = '${msg.author.id}'`, (err, rows) => {
-    if(err) throw err;
-
+    if (err) throw err;
+    console.log(rows);
     let sql;
 
-    if(rows.length < 1) {
+    if (rows.length < 1) {
       sql = `INSERT INTO xp (id, xp) VALUES ('${msg.author.id}', ${generate_xp()})`;
     } else {
       let xp = rows[0].xp;
@@ -30,6 +35,21 @@ function connection() {
     connection.query(sql);
 
   });
-};
+}
 
-module.exports = { connection };
+function xp(msg, args) {
+
+  let target = msg.mentions.users.first() || msg.guild.members.get(args[1]) || msg.author;
+
+  connection.query(`SELECT * FROM xp WHERE id = '${target.id}'`, (err, rows) => {
+    if (err) throw err;
+
+    if (!rows[0]) return msg.channel.send('This User has not gathered any XP.');
+
+    let xp = rows[0].xp;
+    msg.channel.send(xp);
+  });
+
+}
+
+module.exports = { connect, query, xp };
