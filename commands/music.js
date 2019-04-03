e88ec987254ffd26f0966ed651c9f9a68257f1d3
David <david@david-rosseljong.com>
2019-04-03 11:34:09 +0200

    Added stop command

--- commands/music.js
+++ commands/music.js
@@ -4,6 +4,8 @@ module.exports.run = async (client, msg, args) => {
   const { settings } = require('../inc/settings');
   // Importing ytdl-core
   const ytdl = require('ytdl-core');
+  // Turn args into string
+  const argsToString = args.toString();
 
   // Check if settings are enabled
   if (settings.enabled_music) {
@@ -32,6 +34,7 @@ module.exports.run = async (client, msg, args) => {
       console.error(`I could not join because: ${error}`);
     };
 
+    // Get the url and play the music
     const dispatcher = connection.playStream(ytdl(args[0]))
       .on('end', () => {
         console.log('The song has ended!');
@@ -41,8 +44,17 @@ module.exports.run = async (client, msg, args) => {
         console.log(args);
       });
 
+    // Set the initial volume
     dispatcher.setVolume(0.2);
 
+    // If the command is !music stop, then the bot leaves the channel
+    if (argsToString === 'stop' && args !== '') {
+
+      if (!msg.member.voiceChannel) return msg.channel.send('You are not in a voice channel');
+      msg.member.voiceChannel.leave();
+      return undefined;
+
+    };
   
   };
 
