0ff12fd0b6ca707485ae4d624b4bb1612f1dcdc9
David Rosseljong <david@david-rosseljong.com>
2021-03-10 15:34:52 +0100

    First iteration of Role Reactions

---
+++ inc/roles.js
@@ -0,0 +1,62 @@
+const firstMessage = require('./embeds/first-message')
+
+module.exports = (client) => {
+  const channelId = '819202903428169779'
+
+  const getEmoji = (emojiName) =>
+    client.emojis.cache.find((emoji) => emoji.name === emojiName)
+
+  const emojis = {
+    man_mage: 'Raid',
+    man_zombie: 'Social',
+  }
+
+  const reactions = []
+
+  let emojiText = 'Wähle deine Rolle für die Gilde\n\n'
+  for (const key in emojis) {
+    const emoji = getEmoji(key)
+    reactions.push(emoji)
+
+    const role = emojis[key]
+    emojiText += `${emoji} = ${role}\n`
+  }
+
+  firstMessage(client, channelId, emojiText, reactions)
+
+  const handleReaction = (reaction, user, add) => {
+    if (user.id === '542667050917691404') {
+      return
+    }
+
+    const emoji = reaction._emoji.name
+
+    const { guild } = reaction.message
+
+    const roleName = emojis[emoji]
+    if (!roleName) {
+      return
+    }
+
+    const role = guild.roles.cache.find((role) => role.name === roleName)
+    const member = guild.members.cache.find((member) => member.id === user.id)
+
+    if (add) {
+      member.roles.add(role)
+    } else {
+      member.roles.remove(role)
+    }
+  }
+
+  client.on('messageReactionAdd', (reaction, user) => {
+    if (reaction.message.channel.id === channelId) {
+      handleReaction(reaction, user, true)
+    }
+  })
+
+  client.on('messageReactionRemove', (reaction, user) => {
+    if (reaction.message.channel.id === channelId) {
+      handleReaction(reaction, user, false)
+    }
+  })
+}
